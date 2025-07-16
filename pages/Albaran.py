5918c33c4b6f7defc2be35cb2c7969b5b98e2aee
FelipeTevar <felipetevar@gmail.com>
2025-07-16 08:57:45 +0200

    cambios nuevos

--- pages/Albaran.py
+++ pages/Albaran.py
@@ -1,12 +1,18 @@
-
 import streamlit as st
+import time
 
+with st.spinner("Leyendo Documentos..."):
+    for i in range(100):
+        time.sleep(0.005)
+        st.progress(i + 1)
+st.success("¡Comparando Documentos!")
+
+# Luego el contenido de la pantalla
 st.title("Pantalla Albarán")
-
 st.text("Instrucción:")
 archivo = st.file_uploader("Adjuntar Archivo")
 if archivo:
     st.success("Archivo adjuntado correctamente.")
-
 st.text("Datos/Info de los Adjuntos:")
 st.text_area("Información adicional")
+
